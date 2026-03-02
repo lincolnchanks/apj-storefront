9d13c9398961192cea352077900247355743bdbc
lincolnchanks <lchanks2024@gmail.com>
2026-03-02 16:13:59 -0700

    W9 Tutorial 9-1

--- web/src/main/java/edu/byui/apj/storefront/web/controller/CardController.java
+++ web/src/main/java/edu/byui/apj/storefront/web/controller/CardController.java
@@ -2,10 +2,8 @@ package edu.byui.apj.storefront.web.controller;
 
 import edu.byui.apj.storefront.web.model.Card;
 import edu.byui.apj.storefront.web.service.CardService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,20 +11,26 @@ import java.util.List;
 @RequestMapping("/api/cards")
 public class CardController {
 
-    private final CardService cardService;
+    private final CardService service;
 
-    public CardController(CardService cardService){
-        this.cardService = cardService;
+    public CardController(CardService service) {
+        this.service = service;
     }
 
     @GetMapping("/featured")
-    public List<Card> featuredCards(){
-        return cardService.getFeaturedCards();
+    public List<Card> featured(@RequestParam(required = false) String q) {
+        return service.getFeaturedCards(q);
     }
 
-    @GetMapping("/whoami")
-    public String whoAmI(@RequestHeader("User-Agent") String userAgent){
-        return "You are using: " + userAgent;
+    @GetMapping
+    public List<Card> all() {
+        return service.getAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Card> byId(@PathVariable Long id) {
+        return service.getById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
