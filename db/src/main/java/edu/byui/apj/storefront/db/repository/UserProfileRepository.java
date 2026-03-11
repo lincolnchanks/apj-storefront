6354ef0f7cd6b6f67e947e4cbac7fb4895b3caa7
lincolnchanks <lchanks2024@gmail.com>
2026-03-11 16:09:14 -0600

    W10 Tutorial 10-2

---
+++ db/src/main/java/edu/byui/apj/storefront/db/repository/UserProfileRepository.java
@@ -0,0 +1,7 @@
+package edu.byui.apj.storefront.db.repository;
+
+import edu.byui.apj.storefront.db.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+}
